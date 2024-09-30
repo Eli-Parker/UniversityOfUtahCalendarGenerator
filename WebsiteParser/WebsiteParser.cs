67b80f2c13bc23e789a0556f7792c5cf603a1929
Eli Parker <92594528+Eli-Parker@users.noreply.github.com>
2024-09-30 12:28:15 -0600

    Add project files.

---
+++ WebsiteParser/WebsiteParser.cs
@@ -0,0 +1,12 @@
+﻿namespace WebsiteParser;
+
+/// <summary>
+/// <para>
+/// Class which contains functionality to parse calender from the University of Utah's
+/// break schedule.
+/// </para>
+/// </summary>
+public class WebsiteParser
+{
+
+}
