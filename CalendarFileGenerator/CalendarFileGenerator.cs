0d923ed8f820571e47895c012e17efe50bbee956
Eli Parker <92594528+Eli-Parker@users.noreply.github.com>
2024-10-02 15:55:31 -0600

    Started adding methods to CalendarFileGenerator.cs

--- CalendarFileGenerator/CalendarFileGenerator.cs
+++ CalendarFileGenerator/CalendarFileGenerator.cs
@@ -11,5 +11,32 @@ using Ical.Net;
 /// </summary>
 public class CalendarFileGenerator
 {
-    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CalendarFileGenerator"/> class.
+    /// </summary>
+    public CalendarFileGenerator()
+    {
+    }
+
+    /// <summary>
+    /// Export the calendar to a file at the given file-path as a .ics file.
+    /// </summary>
+    /// <returns> True if writing to the given path was successful, false otherwise. </returns>
+    public void ExportCalendarToFilePath()
+    {
+        // TODO IMPLEMENT ME
+    }
+
+    /// <summary>
+    /// Adds a new event to the calendar file.
+    /// </summary>
+    /// <param name="eventName"></param>
+    /// <param name="startTime"></param>
+    /// <param name="endTime"></param>
+    /// <returns></returns>
+    public bool AddCalendarEvent(string eventName, DateOnly startTime, DateOnly endTime)
+    {
+        return false;
+        // TODO IMPLEMENT ME
+    }
 }
