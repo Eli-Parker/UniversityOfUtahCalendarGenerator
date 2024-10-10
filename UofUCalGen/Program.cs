3d88ecd544550916eb745b5c583b8adeb3f57a24
Eli Parker <racecar47@icloud.com>
2024-10-09 23:49:44 -0600

    Added EventSelect.razor and started implementation with nested table

--- UofUCalGen/Program.cs
+++ UofUCalGen/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using UofUCalGen;
-using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
