6e9556d3719b1d7f56cf01a329b405c971b07cfb
Eli Parker <92594528+Eli-Parker@users.noreply.github.com>
2024-10-11 13:49:36 -0600

    Modified CalendarController to accept lists of names AND dates, continued Front end UI work

---
+++ UofUCalGen/Pages/AppState.cs
@@ -0,0 +1,54 @@
+﻿// Ignore Spelling: Uof
+
+using MudBlazor;
+
+namespace UofUCalGen.Pages;
+
+/// <summary>
+/// Holds all variables used by all pages.
+/// </summary>
+public class AppState
+
+{
+    public SiteSelector SiteSelectorComponent
+    {
+        get; set;
+    }
+
+    /// <summary>
+    /// Theme to use for the application.
+    /// </summary>
+    public static MudTheme SchoolSpiritTheme = new()
+    {
+        PaletteLight = new PaletteLight(),
+        PaletteDark = new PaletteDark()
+        {
+            Primary = Colors.Red.Darken3,
+            Secondary = Colors.Gray.Darken1,
+            Tertiary = Colors.Red.Darken4,
+            Background = Colors.Gray.Darken4,
+            AppbarBackground = Colors.Red.Darken4,
+        },
+
+        LayoutProperties = new LayoutProperties()
+        {
+            DrawerWidthLeft = "0px",
+            DrawerWidthRight = "0px"
+        }
+    };
+
+    /// <summary>
+    /// Controls whether the first section of the site is disabled
+    /// </summary>
+    public bool FirstSectionDisabled { get; set; }
+
+    /// <summary>
+    /// Controls whether the drop-down for the second section is expanded
+    /// </summary>
+    public bool SecondSectionExpanded { get; set; }
+
+    /// <summary>
+    /// Gives the second page the path to the chosen file.
+    /// </summary>
+    public string? ChosenPath { get; set; }
+}
